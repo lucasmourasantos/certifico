132532e440c74677b6f53a234642a5fa333d1517
lucasmourasantos <lucasmourasantos@gmail.com>
2019-06-22 20:47:55 -0300

    Alterações falta apenas o BD

--- app/Views/users.public.php
+++ app/Views/users.public.php
@@ -1,5 +1,5 @@
-<?php include_once('app/Views/parts/header-public.php'); ?>
+<?php include_once('parts/header-public.php'); ?>
     <div class="starter-template">
         <img class="img-fluid mb-4" src="app\Views\img\Logo Certifico.png" alt="CERTIFICO | Certificado Online" width="" height="">
     </div>
-<?php include_once('app/Views/parts/footer.php'); ?>
+<?php include_once('parts/footer.php'); ?>
