26b61c156ccbd7cc5672e88418b6bd92bede8310
lucasmourasantos <lucasmourasantos@gmail.com>
2019-06-23 18:27:58 -0300

    Alterações nas credenciais do BD

--- app/DB.php
+++ app/DB.php
@@ -4,25 +4,6 @@ namespace app;
 
 class DB extends \PDO
 {
-  $url = parse_url(getenv("mysql://b5ee2adc25aa24:2ed683a3@us-cdbr-iron-east-02.cleardb.net/heroku_78a881d13ca0c35?reconnect=true"));
-
-  $server = $url["us-cdbr-iron-east-02.cleardb.net"];
-  $username = $url["b5ee2adc25aa24"];
-  $password = $url["2ed683a3"];
-  $db = substr($url["heroku_78a881d13ca0c35"], 1);
-
-  /*
-  $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-  $conn = new mysqli($server, $username, $password, $db);
-  */
-
-  // credenciais de acesso ao MySQL
-  define('MYSQL_HOST', $server);
-  define('MYSQL_USER', $username);
-  define('MYSQL_PASS', $password);
-  define('MYSQL_DBNAME', $db);
-
     public function __construct($dsn = null, $username = null, $password = null, $options = array())
     {
         $dsn = ($dsn != null) ? $dsn : sprintf('mysql:dbname=%s;host=%s', MYSQL_DBNAME, MYSQL_HOST);
@@ -32,35 +13,3 @@ class DB extends \PDO
         parent::__construct($dsn, $username, $password, $options);
     }
 }
-
-/*
-private $server;
-   private $database;
-   private $username;
-   private $password;
-
-   function __construct(){
-       $url = parse_url(getenv("mysql://b5ee2adc25aa24:2ed683a3@us-cdbr-iron-east-02.cleardb.net/heroku_78a881d13ca0c35?reconnect=true"));
-
-       $this->$server = $url["us-cdbr-iron-east-02.cleardb.net"];
-       $this->$username = $url["b5ee2adc25aa24"];
-       $this->$password = $url["2ed683a3"];
-       $this->$database = substr($url["heroku_78a881d13ca0c35"], 1);
-
-   }
-
-   function connection(){
-       mysqli_report(MYSQLI_REPORT_STRICT);
-
-       try{
-           $mysqli = new mysqli($this->server, $this->username, $this->password, $this->database);
-       }
-       catch (Exception $e ) {
-           echo file_get_contents("Pagine/ContattiInizio.xhtml");
-           echo "message: " . $e->getMessage();   //This show a "no such a file or directory" error
-           exit;
-       }
-
-       return $mysqli;
-   }
-*/
